590dc560c3b3d87a78fe83c368acd466a1d80418
kurdikerinick <nikhita111999@gmail.com>
2025-06-17 11:34:19 +0530

    Fixed register query and input

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,5 +1,4 @@
 // controllers/authController.js
-
 const pool = require('../db');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
@@ -10,14 +9,34 @@ exports.register = async (req, res) => {
   try {
     const hashed = await bcrypt.hash(password, 10);
 
-    const result = await pool.query(
+    await pool.query(
       'INSERT INTO users (name, email, password) VALUES ($1, $2, $3)',
       [name, email, hashed]
     );
 
     res.json({ message: 'User registered successfully' });
   } catch (err) {
-    console.error('Error during register:', err);
+    console.error('Error in register:', err);
+    res.status(400).json({ error: err.message });
+  }
+};
+
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const result = await pool.query('SELECT * FROM users WHERE email=$1', [email]);
+    const user = result.rows[0];
+
+    if (!user) return res.status(404).json({ error: 'User not found' });
+
+    const valid = await bcrypt.compare(password, user.password);
+    if (!valid) return res.status(401).json({ error: 'Invalid credentials' });
+
+    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET);
+    res.json({ message: 'Login successful', token });
+  } catch (err) {
+    console.error('Error in login:', err);
     res.status(400).json({ error: err.message });
   }
 };
